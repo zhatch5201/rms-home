890102e592a491e82c1d7e289f7534a85d030a7a
Taylor <zodd515@gmail.com>
2021-02-18 15:23:20 -0700

    base table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import PeopleGrid from './firebase/people_grid';
 import PersonCard from './firebase/person_card';
+import PeopleList from './firebase/people_list';
 import './App.css';
 
 function App() {
 
-  return (<div><PersonCard /><br /></div>);
+  return (<div><PeopleList /><br /></div>);
 }
 
 export default App;
