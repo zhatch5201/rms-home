a28f6f57463471aa1cf26cfd7b329cf6e452b9fd
Hunter Funk <hfunk336@west-mec.org>
2021-02-18 15:24:01 -0700

    began working on ui elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import PeopleGrid from './firebase/people_grid';
 import PersonCard from './firebase/person_card';
+import BottomNavbar from './firebase/bottom_navbar';
 
 import './App.css';
 
 function App() {
 
-  return (<div><PersonCard /><br /></div>);
+  return (<div><BottomNavbar /><br /></div>);
 }
 
 export default App;
