0b11176555c9d738f12f6a8e02faf1d9021e177f
Taylor <zodd515@gmail.com>
2021-02-22 15:08:31 -0700

    fixed I think sorry for messing with master zack

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import GetPeople from './Components/get_people';
+import PeopleGrid from './Components/people_grid';
+import PersonCard from './Components/person_card';
 import BottomNavbar from './firebase/bottom_navbar';
 import NavBar from './firebase/nav_bar'
 import './App.css';
