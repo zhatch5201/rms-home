fa1230ed361ecbf4ead5f5ff2074bd06b8d4cb21
Taylor <zodd515@gmail.com>
2021-02-22 15:03:38 -0700

    fixed things causing issues

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import PeopleGrid from './firebase/people_grid';
-import PersonCard from './firebase/person_card';
-<<<<<<< HEAD
 import BottomNavbar from './firebase/bottom_navbar';
-
-=======
 import NavBar from './firebase/nav_bar'
->>>>>>> Front-End-Skeleton
 import './App.css';
 
 function App() {
-
-  return (
-<<<<<<< HEAD
-    <div>
-      <PeopleGrid /><br/>
-    </div>
-    
-    );
-=======
+  return(
   <div>
+    <BottomNavbar /><br/>
      <NavBar /><br />
   </div>
   );
->>>>>>> Front-End-Skeleton
 }
 
 export default App;
