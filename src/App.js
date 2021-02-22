374c693b93cfbaf9921c7a55238de0614938b62c
Taylor <zodd515@gmail.com>
2021-02-22 15:15:43 -0700

    files need to be fixed before I can continue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import BottomNavbar from './firebase/bottom_navbar';
+import GetPeople from './Components/get_people';
+import PeopleGrid from './Components/people_grid';
+import PersonCard from './Components/person_card';
 import NavBar from './firebase/nav_bar'
 import './App.css';
 
 function App() {
   return(
   <div>
-    <BottomNavbar /><br/>
      <NavBar /><br />
+     <PeopleGrid /><br />
   </div>
   );
 }
