783ee30c811c8763ce7f074c791db079f10bf8b0
Hunter Funk <hfunk336@west-mec.org>
2021-02-22 15:04:53 -0700

    started incidence form component

---
+++ src/Components/incidence_form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
