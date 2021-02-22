44552fcb4427fef4c9ccc3dee35363fddda355e3
Hunter Funk <hfunk336@west-mec.org>
2021-02-22 15:21:25 -0700

    Starting incidence form page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import PeopleGrid from './Components/people_grid';
 import PersonCard from './Components/person_card';
 import BottomNavbar from './firebase/bottom_navbar';
+import NavBar from './firebase/nav_bar';
+import IncidenceForm from './Components/incidence_form';
 
 import './App.css';
 
@@ -9,6 +11,7 @@ function App() {
   return (
   <div>
      <NavBar /><br />
+     <IncidenceForm/><br/>
   </div>
   );
 }
--- src/Components/incidence_form.js
+++ src/Components/incidence_form.js
@@ -10,3 +10,44 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 }));
+export default function FormPropsTextFields() {
+    const classes = useStyles();
+  
+    return (
+      <form className={classes.root} noValidate autoComplete="off">
+        <div>
+          <TextField required id="standard-required" label="Required" defaultValue="Hello World" />
+          <TextField disabled id="standard-disabled" label="Disabled" defaultValue="Hello World" />
+          <TextField
+            id="standard-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+          />
+          <TextField
+            id="standard-read-only-input"
+            label="Read Only"
+            defaultValue="Hello World"
+            InputProps={{
+              readOnly: true,
+            }}
+          />
+          <TextField
+            id="standard-number"
+            label="Number"
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField id="standard-search" label="Search field" type="search" />
+          <TextField
+            id="standard-helperText"
+            label="Helper text"
+            defaultValue="Default Value"
+            helperText="Some important text"
+          />
+        </div>
+        </form>
+    );
+}
