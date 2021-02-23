6710685177910b4d300afc6608b68452e499ce10
Hunter Funk <hfunk336@west-mec.org>
2021-02-23 15:08:26 -0700

    Removed BottomNavbar from app.js, added js for people form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import GetPeople from './Components/get_people';
 import PeopleGrid from './Components/people_grid';
 import PersonCard from './Components/person_card';
-import BottomNavbar from './firebase/bottom_navbar';
 import NavBar from './firebase/nav_bar'
 import './App.css';
 
 function App() {
   return(
   <div>
-    <BottomNavbar /><br/>
      <NavBar /><br />
   </div>
   );
