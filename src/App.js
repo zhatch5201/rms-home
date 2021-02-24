d5e24caa9e3039ebd5687c638d8f23d475bcd5ed
Hunter Funk <hfunk336@west-mec.org>
2021-02-24 14:44:22 -0700

    started working on people_form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import GetPeople from './Components/get_people';
 import PeopleGrid from './Components/people_grid';
 import PersonCard from './Components/person_card';
-import NavBar from './firebase/nav_bar'
-import IncidenceList from './Components/incidence_list'
+import NavBar from './firebase/nav_bar';
+import IncidenceList from './Components/incidence_list';
+import PeopleForm from './Components/people_form';
 import './App.css';
 
 function App() {
   return(
   <div>
      <NavBar /><br />
-     <IncidenceList /><br />
+     <PeopleForm /><br />
   </div>
   );
 }
