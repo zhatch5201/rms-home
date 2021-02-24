baa650afcd12a7eb88b7d0e290525e1884c405a7
Zackery Hatch <zackery.hatch@yahoo.com>
2021-02-24 03:14:57 -0700

    Added the stuff for form hooks to the incident page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import PeopleGrid from './Components/people_grid';
 import PersonCard from './Components/person';
 import NavigationBar from './Components/navbar';
+import IncidenceForm from './Components/incidence_form';
 // Router
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-// Firebase
-import firebase from 'firebase';
-import { UserContext } from './Components/UserContext';
-import { useContext } from 'react';
 
 function App() {
 
@@ -16,7 +13,7 @@ function App() {
         <NavigationBar />
         <Switch>
           <Route path='/people' component={PersonCard} />
-          {/* <Route path='/incidents' component={ } /> */}
+          <Route path='/incidents' component={IncidenceForm} />
           <Route path='/home' exact component={PeopleGrid} />
           <Route path='/' exact component={PeopleGrid} />
         </Switch>
