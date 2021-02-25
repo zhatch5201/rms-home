50fe2ac673f4b85b2385ade109859e4d74cb8915
Taylor <zodd515@gmail.com>
2021-02-25 14:05:59 -0700

    Vehicle list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import PeopleGrid from './Components/people_grid';
-import PersonCard from './Components/person';
+import NewNav from './Components/nav_bar';
 import NavigationBar from './Components/navbar';
-import IncidenceForm from './Components/incidence_form';
-import IncidenceList from './Components/incidence_list';
 import IncidencePage from './Components/indicence_page';
 import PeoplePage from './Components/people_page';
 import VehiclePage from './Components/vehicle_page';
-import NewNav from './Components/nav_bar';
+import IncidenceList from './Components/incidence_list';
+import PeopleGrid from './Components/people_grid';
+import VehicleList from './Components/vehicle_list'
+import PersonCard from './Components/person';
+import IncidenceForm from './Components/incidence_form';
 // Router
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
@@ -17,7 +18,7 @@ function App() {
       <Router>
         <NewNav />
         <Switch>
-          <Route path='/people' component={PeoplePage} />
+          <Route path='/people' component={VehicleList} />
           <Route path='/incidents' component={IncidencePage} />
           <Route path='/home' exact component={PeopleGrid} />
           <Route path='/' exact component={PeopleGrid} />
