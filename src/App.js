3a371bd92e93e98b259deab1bd0ef609b5450715
Taylor <zodd515@gmail.com>
2021-02-25 13:37:11 -0700

    simple landing pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import PersonCard from './Components/person';
 import NavigationBar from './Components/navbar';
 import IncidenceForm from './Components/incidence_form';
 import IncidenceList from './Components/incidence_list';
+import IncidencePage from './Components/indicence_page';
+import PeoplePage from './Components/people_page';
+import VehiclePage from './Components/vehicle_page';
 import NewNav from './Components/nav_bar';
 // Router
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
@@ -14,8 +17,8 @@ function App() {
       <Router>
         <NewNav />
         <Switch>
-          <Route path='/people' component={PersonCard} />
-          <Route path='/incidents' component={IncidenceForm} />
+          <Route path='/people' component={PeoplePage} />
+          <Route path='/incidents' component={IncidencePage} />
           <Route path='/home' exact component={PeopleGrid} />
           <Route path='/' exact component={PeopleGrid} />
         </Switch>
