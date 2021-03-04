8b1c672245864aaa06c71f815a8a8536e3cbf5f9
Hunter Funk <hfunk336@west-mec.org>
2021-03-04 14:31:04 -0700

    added all objects for database entry

--- src/Components/incident_form.js
+++ src/Components/incident_form.js
@@ -83,7 +83,7 @@ export default function FormPropsTextFields() {
           first_name: submittedForm.FirstName,
           middle_name: submittedForm.MiddleName,
           relevance: submittedForm.relevance
-        }
+        },
       }],
       vehicles_involved: [{
           vehicle: {
