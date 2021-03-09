9628d4cb45e1367b18131375354f03bf16ffcce8
Ashlynn Hickman <ashlynnh02@gmail.com>
2021-03-09 13:32:30 -0700

    fixed layering issue just for hunter :)

--- src/Components/vehicle_list.js
+++ src/Components/vehicle_list.js
@@ -37,7 +37,7 @@ const rows = [
 
 export default function DataTable() {
   return (
-    <div id="pepe" style={{ height: 400, width: '100%' }}>
+    <div id="pepe" style={{ height: 400, width: '100%'}}>
       <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
     </div>
   );
